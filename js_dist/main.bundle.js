188037cfa46681509ec98fae5c6b02b5fbe11c48
kawa <katarzyna.walenko@21s.pl>
2018-01-07 21:44:43 +0100

    #3: run js bundle

--- js_dist/main.bundle.js
+++ js_dist/main.bundle.js
@@ -65,13 +65,53 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__runLogoAnimation__ = __webpack_require__(2);
 
 
-console.log('jest bundle, ale czy się zmienia?');
-console.log('zmienia się, zmienia');
+$(document).ready(() => {
+
+    Object(__WEBPACK_IMPORTED_MODULE_0__runLogoAnimation__["a" /* default */])();
+    console.log('animacja działa? działa!');
+});
+
+/***/ }),
+/* 1 */,
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (immutable) */ __webpack_exports__["a"] = runLogoAnimation;
+function runLogoAnimation() {
+
+    // GSAP vars https://greensock.com/docs/#/HTML5/GSAP/TimelineLite/
+    var tl = new TimelineLite();
+
+    //---------------
+    // LOGO ANIMATION
+    //---------------
+    // logo vars
+    var $fullLogos = $('#sketch > *, #logo-border, #logo-color');
+    var $logoSketch = $('#sketch');
+    var $base1 = $('#base1');
+    var $base2 = $('#base2');
+    var $base3 = $('#base3');
+    var $lineSet1 = $('#line2, #line20, #line22, #line14');
+    var $lineSet2 = $('#line4, #line30, #line12, #line15');
+    var $lineSet3 = $('#line10, #line23, #line24, #line16');
+    var $lineSet4 = $('#line13, #line27, #line29, #line21');
+    var $lineSet5 = $('#line1, #line3, #line5, #line25');
+    var $lineSet6 = $('#line9, #line19, #line28');
+    var $lineSet7 = $('#line6, #line7, #line8, #line31');
+    var $lineSet8 = $('#line11, #line17, #line18, #line26');
+    var $logoBorder = $('#logo-border');
+    var $logoColor = $('#logo-color');
+
+    tl.to($fullLogos, 0, { opacity: 0 }).to($base1, 1, { opacity: 1, ease: Linear.easeOut }).to($base2, 1, { opacity: 1, ease: Linear.easeOut }).to($base3, 1, { opacity: 1, ease: Linear.easeOut }).to($lineSet1, .9, { opacity: 1, ease: Linear.easeOut }, '-=.1').to($lineSet2, .9, { opacity: 1, ease: Linear.easeOut }, '-=.3').to($lineSet3, .9, { opacity: 1, ease: Linear.easeOut }, '-=.3').to($lineSet4, .9, { opacity: 1, ease: Linear.easeOut }, '-=.3').to($lineSet5, .9, { opacity: 1, ease: Linear.easeOut }, '-=.3').to($lineSet6, .9, { opacity: 1, ease: Linear.easeOut }, '-=.3').to($lineSet7, .9, { opacity: 1, ease: Linear.easeOut }, '-=.3').to($lineSet8, .9, { opacity: 1, ease: Linear.easeOut }, '-=.3').addLabel('logoBorder', '+=2').to($logoBorder, 2, { opacity: 1, ease: Linear.easeOut }, 'logoBorder').addLabel('logoColor', '+=2.5').to($logoColor, 2, { opacity: 1, ease: Linear.easeOut }, 'logoColor').addLabel('sketchFade', '-=.5').to($logoBorder, .5, { opacity: 0 }).to($logoSketch, 3, { opacity: 0 }, 'sketchFade');
+};
 
 /***/ })
 /******/ ]);
