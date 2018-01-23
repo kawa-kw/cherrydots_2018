1961dc68859a3bf0c856e767dd8dbb70b0561c5c
kawa <katarzyna.walenko@21s.pl>
2018-01-24 00:02:47 +0100

    ^3: warstwy się ruszają!

--- _js/main.js
+++ _js/main.js
@@ -4,6 +4,9 @@ import moveBcgLayer from './moveBcgLayer';
 $(document).ready(() => {
 
     runLogoAnimation();
-    moveBcgLayer($(window), $('#dev-bcg-intro-color #bcg-1'), 1);
+    moveBcgLayer($(window), $('#dev-bcg-intro-color #bcg-1'), 6);
+    moveBcgLayer($(window), $('#dev-bcg-intro-color #bcg-2'), 8);
+    moveBcgLayer($(window), $('#dev-bcg-intro-color #bcg-3'), 10);
+    moveBcgLayer($(window), $('#dev-bcg-intro-color #bcg-4'), 12);
     console.log('animacja działa? działa!');
 });
--- _js/moveBcgLayer.js
+++ _js/moveBcgLayer.js
@@ -1,33 +1,5 @@
-// /*
-// * jquery-backgroundMove master by sameera liyanage
-// * License MIT
-// $('element').backgroundMove();
-// */
-// (function($){
-//     $.fn.backgroundMove=function(options){
-//             var defaults={
-//             movementStrength:'50'
-//         },
-//         options=$.extend(defaults,options);
-//
-//          var $this = $(this);
-//
-//            var movementStrength = options.movementStrength;
-//             var height = movementStrength / $(window).height();
-//             var width = movementStrength / $(window).width();
-//             $this.mousemove(function(e){
-//                       var pageX = e.pageX - ($(window).width() / 2);
-//                       var pageY = e.pageY - ($(window).height() / 2);
-//                       var newvalueX = width * pageX * -1 - 25;
-//                       var newvalueY = height * pageY * -1 - 50;
-//                       $this.css("background-position", newvalueX+"px     "+newvalueY+"px");
-//             });
-//
-//         }
-// })(jQuery);
-//
 export default function moveBcgLayer(bcg, layer, movementStrength){
-    var window = $(window);
+    var window = $(document);
     var dx = 0 // number of px to move layer on X axis
     var dy = 0 // number of px to move layer on Y axis
     var factor = 1 / (30 * movementStrength)
@@ -36,23 +8,31 @@ export default function moveBcgLayer(bcg, layer, movementStrength){
         var newX = window.width() / 2 - event.clientX
         var newY = window.height() / 2 - event.clientY
 
-        console.log('moveBcgLayer działa', newX, newY)
+        function moveSingleLayer(){
+            dx += (newX - dx) * factor
+            dy += (newY - dy) * factor
+
+            if ( Math.abs(dx) >= 50 ){
+                dx = dx;
+                console.log('juz x jest max')
+            }
+
+            if ( Math.abs(dy) >= 50 ){
+                dy = dy;
+                console.log('juz y jest max')
+            }
+
+            console.log(dx, dy)
+
+            var translate = 'translate(' + dx + 'px, ' + dy + 'px)';
+
+            layer.css({
+                '-webit-transform': translate,
+                '-moz-transform': translate,
+                'transform': translate
+            });
+        }
+
+        moveSingleLayer();
     });
-
-    function moveSingleLayer(){
-        dx += ((newX * (-1)) - dx) * factor
-        dy += ((newY * (-1)) - dy) * factor
-
-        translate = 'translate(' + dx + 'px, ' + dy + 'px) scale(1.1)';
-
-        layer.css({
-            '-webit-transform': translate,
-            '-moz-transform': translate,
-            'transform': translate
-        });
-
-        window.requestAnimationFrame(moveSingleLayer);
-        console.log('moveSingleLayer działa')
-    }
-
 }
