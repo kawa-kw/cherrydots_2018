54d64b4f31d06020cf0433d59ec11a25927106bf
kawa <katarzyna.walenko@21s.pl>
2018-01-27 01:12:01 +0100

    ^3: intro finisz

--- _js/moveBcgLayer.js
+++ _js/moveBcgLayer.js
@@ -2,27 +2,24 @@ export default function moveBcgLayer(bcg, layer, movementStrength){
     var window = $(document);
     var dx = 0 // number of px to move layer on X axis
     var dy = 0 // number of px to move layer on Y axis
-    var factor = 1 / (30 * movementStrength)
+    var factor = 1 / (3 * movementStrength)
 
     bcg.mousemove(function( event ) {
         var newX = window.width() / 2 - event.clientX
         var newY = window.height() / 2 - event.clientY
 
         function moveSingleLayer(){
-            dx += (newX - dx) * factor
-            dy += (newY - dy) * factor
-
-            if ( Math.abs(dx) >= 50 ){
-                dx = dx;
-                console.log('juz x jest max')
+            if ( Math.abs(dx) >= 100 ){
+                dx = 0;
+            } else {
+                dx = (newX - dx) * factor
             }
 
             if ( Math.abs(dy) >= 50 ){
-                dy = dy;
-                console.log('juz y jest max')
+                dy = 0;
+            } else {
+                dy = (newY - dy) * factor
             }
-
-            console.log(dx, dy)
 
             var translate = 'translate(' + dx + 'px, ' + dy + 'px)';
 
