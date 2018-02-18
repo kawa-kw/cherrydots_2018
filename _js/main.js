ed498a4fefd157058f01c40082bb8546714bf121
kawa <katarzyna.walenko@21s.pl>
2018-02-18 23:16:13 +0100

    #5: fix intro paralax

--- _js/main.js
+++ _js/main.js
@@ -4,8 +4,8 @@ import moveBcgLayer from './moveBcgLayer';
 $(document).ready(() => {
 
     runLogoAnimation();
-    moveBcgLayer($('.js-intro-bcg-shapes'), $('#dev-bcg-intro-color #bcg-1'), 10);
-    moveBcgLayer($('.js-intro-bcg-shapes'), $('#dev-bcg-intro-color #bcg-2'), 10);
-    moveBcgLayer($('.js-intro-bcg-shapes'), $('#dev-bcg-intro-color #bcg-3'), 30);
+    moveBcgLayer($('body'), $('#dev-bcg-intro-color #bcg-1'), 20);
+    moveBcgLayer($('body'), $('#dev-bcg-intro-color #bcg-2'), 20);
+    moveBcgLayer($('body'), $('#dev-bcg-intro-color #bcg-3'), 30);
     console.log('animacja działa? działa!');
 });
