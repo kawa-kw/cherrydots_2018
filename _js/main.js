bf57b1e5148804626032a438ceb30f805b4edc78
kawa <katarzyna.walenko@21s.pl>
2018-06-20 09:46:54 +0200

    ^8: szybciej animacjo

--- _js/main.js
+++ _js/main.js
@@ -4,8 +4,8 @@ import moveBcgLayer from './moveBcgLayer';
 $(document).ready(() => {
 
     runLogoAnimation();
-    moveBcgLayer($('.js-intro-bcg-shapes'), $('#dev-bcg-intro-color #bcg-1'), 20);
-    moveBcgLayer($('.js-intro-bcg-shapes'), $('#dev-bcg-intro-color #bcg-2'), 20);
-    moveBcgLayer($('.js-intro-bcg-shapes'), $('#dev-bcg-intro-color #bcg-3'), 30);
+    // moveBcgLayer($('.js-intro-bcg-shapes'), $('#dev-bcg-intro-color #bcg-1'), 20);
+    // moveBcgLayer($('.js-intro-bcg-shapes'), $('#dev-bcg-intro-color #bcg-2'), 20);
+    // moveBcgLayer($('.js-intro-bcg-shapes'), $('#dev-bcg-intro-color #bcg-3'), 30);
     console.log('animacja działa? działa!');
 });
